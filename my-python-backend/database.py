7ecf92d85a8ba42d520e7aa9ffa052f1ec1260d8
raunitsingh <raunits2006@gmail.com>
2025-01-03 20:06:18 +0000

    more trouble

--- my-python-backend/database.py
+++ my-python-backend/database.py
@@ -7,8 +7,7 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL environment variable is not set")
+print(DATABASE_URL)
 
 database = Database(DATABASE_URL)
 metadata = MetaData()
